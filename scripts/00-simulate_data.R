76dd73f64a1dd14225b2fd7b9d69e169e47ef312
tonytto <taejunjumm@gmail.com>
2024-11-25 06:03:26 -0500

    Edited preamble

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,6 +1,6 @@
 #### Preamble ####
-# Purpose: Simulates a dataset of Australian electoral divisions, including the 
-  #state and party that won each division.
+# Purpose: Creates a random dataset with sucide mortality rate, gdp per capita, 
+# unemployment rate, youth NEET rate, and alcohol consumption for 217 nations.
 # Author: Taejun Um
 # Date: 26 November 2024
 # Contact: taejun.um@utoronto.ca
@@ -17,13 +17,13 @@ set.seed(4020)
 
 num_country = 217
 
-# Create a dataset by randomly assigning states and parties to divisions
+# Create a dataset
 analysis_data <- tibble(
-  country = paste("Country", 1:num_country),  # Add "Division" to make it a character
+  country = paste("Country", 1:num_country),
   suicide_rate = round(rnorm(num_country, 10, 1), 1),
   gdp_per_capita = rlnorm(num_country, 9.5, 0.6),
   unemploy_rate = round(rnorm(num_country, 10, 1), 3),
-  child_exclude_rate = round(rnorm(num_country, 15, 1), 3),
+  youth_neet_rate = round(rnorm(num_country, 15, 1), 3),
   alocohol_consumption =round(rnorm(num_country, 5, 1), 2)
 )
 
