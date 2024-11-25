6a1712c7f7e17397dd355dea9ea7907d06019de9
tonytto <taejunjumm@gmail.com>
2024-11-25 05:58:43 -0500

    Create a simulation data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,50 +1,30 @@
 #### Preamble ####
 # Purpose: Simulates a dataset of Australian electoral divisions, including the 
   #state and party that won each division.
-# Author: Rohan Alexander
-# Date: 26 September 2024
-# Contact: rohan.alexander@utoronto.ca
+# Author: Taejun Um
+# Date: 26 November 2024
+# Contact: taejun.um@utoronto.ca
 # License: MIT
 # Pre-requisites: The `tidyverse` package must be installed
-# Any other information needed? Make sure you are in the `starter_folder` rproj
 
 
 #### Workspace setup ####
 library(tidyverse)
-set.seed(853)
+set.seed(4020)
 
 
 #### Simulate data ####
-# State names
-states <- c(
-  "New South Wales",
-  "Victoria",
-  "Queensland",
-  "South Australia",
-  "Western Australia",
-  "Tasmania",
-  "Northern Territory",
-  "Australian Capital Territory"
-)
 
-# Political parties
-parties <- c("Labor", "Liberal", "Greens", "National", "Other")
+num_country = 217
 
 # Create a dataset by randomly assigning states and parties to divisions
 analysis_data <- tibble(
-  division = paste("Division", 1:151),  # Add "Division" to make it a character
-  state = sample(
-    states,
-    size = 151,
-    replace = TRUE,
-    prob = c(0.25, 0.25, 0.15, 0.1, 0.1, 0.1, 0.025, 0.025) # Rough state population distribution
-  ),
-  party = sample(
-    parties,
-    size = 151,
-    replace = TRUE,
-    prob = c(0.40, 0.40, 0.05, 0.1, 0.05) # Rough party distribution
-  )
+  country = paste("Country", 1:num_country),  # Add "Division" to make it a character
+  suicide_rate = round(rnorm(num_country, 10, 1), 1),
+  gdp_per_capita = rlnorm(num_country, 9.5, 0.6),
+  unemploy_rate = round(rnorm(num_country, 10, 1), 3),
+  child_exclude_rate = round(rnorm(num_country, 15, 1), 3),
+  alocohol_consumption =round(rnorm(num_country, 5, 1), 2)
 )
 
 
