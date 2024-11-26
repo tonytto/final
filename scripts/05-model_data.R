2fb2f7c95f1c7f4e80955bd7974788dc8ad489ae
tonytto <taejunjumm@gmail.com>
2024-11-26 15:38:40 -0500

    rename script files and delete unnecessary files

--- scripts/05-model_data.R
+++ scripts/05-model_data.R
@@ -1,11 +1,10 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Create model for suicide mortality rate with four predictor variables.
+# Author: Taejun Um
+# Date: 26 November 2024
+# Contact: taejun.um@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Data must be downloaded and run 02-clean_data.R 
 
 
 #### Workspace setup ####
