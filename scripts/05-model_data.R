a2ce60bbcd38dd9a21e8e1b322e11dab8a776300
tonytto <taejunjumm@gmail.com>
2024-12-03 23:26:11 -0500

    final

--- scripts/05-model_data.R
+++ scripts/05-model_data.R
@@ -16,7 +16,7 @@ library(arrow)
 cleaned_data2 <- read_parquet("data/02-analysis_data/cleaned_suicide_data.parquet")
 
 ### Model data ####
-first_model <-
+model <-
   stan_glm(
     formula = suicide_rate ~ gdp_per_capita + unemploy_rate + youth_neet_rate + alcohol_consumption,
     data = cleaned_data2,
@@ -24,15 +24,12 @@ first_model <-
     prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
+    seed = 5
   )
-
-lm(suicide_rate ~ gdp_per_capita + unemploy_rate + youth_neet_rate + alcohol_consumption,
-   data = cleaned_data2)
 
 #### Save model ####
 saveRDS(
-  first_model,
+  model,
   file = "models/first_model.rds"
 )
 
